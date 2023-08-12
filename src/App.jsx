03b2dbcecbcf1deb933930c006b4248295b9cd7c
abhishek72340 <abhisingh.72340@gmail.com>
2023-08-12 23:27:04 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import './App.css'
+import Departments from "./pages/Departments.jsx";
+import Products from "./pages/Products.jsx";
+import NewProductPage from "./pages/NewProductPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 
-export default function App() {
-  return <div>App</div>;
-}
+const App = () => {
+    return (
+        <div className='app-main flex gap-6'>
+            <Sidebar />
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/departments' element={<Departments />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/products-details/:id' element={<ProductDetails />} />
+                <Route path='/add-product' element={<NewProductPage />} />
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
